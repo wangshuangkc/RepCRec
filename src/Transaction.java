86eed3d91d937923bec555d6471ab45d730d085b
wangshuangkc <sw2553@nyu.edu>
2016-12-01 23:53:05 -0500

    made small changes

--- src/Transaction.java
+++ src/Transaction.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 /**
  * Transaction object storing
@@ -10,14 +12,14 @@ public class Transaction {
   private final int _number;
   private final int _startTimestamp;
   public final boolean _readOnly;
-  private final List<Operation> _operations;
+  private final Queue<Operation> _pendingOperations;
   private int _endTimeStamp;
 
   public Transaction(int number, int timestamp, boolean readOnly) {
     _number = number;
     _startTimestamp = timestamp;
     _readOnly = readOnly;
-    _operations = copy ArrayList<>();
+    _pendingOperations = new LinkedList<>();
   }
 
   /**
@@ -33,6 +35,6 @@ public class Transaction {
    * @param op copy operation
    */
   public void addOperation(Operation op) {
-    _operations.add(op);
+    _pendingOperations.add(op);
   }
 }
